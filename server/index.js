69e8024f106f279946637f28223a9c4d6e39ce84
priya-aisu <priya.aisu18@gmail.com>
2024-08-14 09:49:32 +0530

    Add new feature

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const{ graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const port=process.env.PORT || 5000;
 const app=express();
-
+//hello
 app.use(
     '/graphql', 
     graphqlHTTP({
